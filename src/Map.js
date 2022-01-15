041d33d21f5d58bffa817d6b091224a438f55000
JizongL <2015rpro@gmail.com>
2022-01-15 10:04:12 -0500

    react setup

---
+++ src/Map.js
@@ -0,0 +1,8 @@
+import "./styles.css";
+import React from "react";
+
+function Map() {
+  return <div>test</div>;
+}
+
+export default Map;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "leaflet/dist/leaflet.css";
+import Map from "./Map";
+import "./styles.css";
+
+function App() {
+  return <Map />;
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
